bd46078ccede481e0f4806a98c9a91faddd6ba20
arybin <arybin@163.com>
2017-01-29 16:34:17 +0800

    structured

---
+++ lib/sdo/account.rb
@@ -0,0 +1,7 @@
+require "sdo/account/version"
+
+module Sdo
+  module Account
+    # Your code goes here...
+  end
+end
---
+++ lib/sdo/account/version.rb
@@ -0,0 +1,5 @@
+module Sdo
+  module Account
+    VERSION = "0.1.0"
+  end
+end
